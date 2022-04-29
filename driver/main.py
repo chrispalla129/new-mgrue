0404a9c63fb89b459828f3697c572356895472a3
chrispalla129 <chrispalla129@gmail.com>
2022-04-29 02:29:32 -0400

    Fixed USB Transfer Implementation

--- driver/main.py
+++ driver/main.py
@@ -1,17 +1,19 @@
 import sys
+import time
+import threading
 
 from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import QQmlApplicationEngine
 from PySide2.QtCore import QObject, Slot, Signal, QTimer
 
-from time import strftime, localtime
+from datetime import datetime
 import serial
 
-global fileCounter
-fileCounter = 0
+
 # Define our backend object, which we will pass to the engine object
 class Backend(QObject):
-
+    recordsPerFile = 500        # Set max number of records that will be written to each file here
+    currentStatus = ""
     updated = Signal(str, arguments=['time'])
     status = Signal(str)
 
@@ -23,53 +25,75 @@ class Backend(QObject):
     # This function is sending data to the frontend (uses the status signal)
     def update_status(self, msg):
         # Pass the current status message to QML.
+        backend.currentStatus = msg
         self.status.emit(msg)
+        
 
     # This function is getting data from frontend
     @Slot(str)
     def getFileLocation(self, location):
-        print("User selected: " + location)
-        self.destination_folder = location
+        print("User selected: " + location[7:])
+        self.destination_folder = location[7:]
 
     def readSerial(self):
-        with serial.Serial('COM1', 19200, timeout=1) as ser:
-            self.update_status("Awaiting Connection")
-            if (ser.inWaiting() > 0):
-                bytesMessage = ser.readline().decode()   # read a '\n' terminated line
+        with serial.Serial('/dev/pts/1', 256000, timeout=1) as ser:
+            while (True):
+                stillReading    = True
+                curTime         = datetime.now().strftime("%H-%M-%S")
+                sequenceNum     = 0
+                fileCounter     = 0
+                bytesMessage    = ser.readline().decode()[:-1]     # read a '\n' terminated line, removing the \n
 
-                if bytesMessage == 'connect':
+                if bytesMessage == 'connect' and self.destination_folder != "":
                     ser.write(b'handshake')
                     self.update_status("Device Connected")
+                    file = open(self.destination_folder + "/" + curTime + "_file" + str(fileCounter) + ".fn", "w")
 
-                    if (ser.inWaiting() > 0):
-                        bytesMessage = ser.readline().decode()
+                    while (stillReading or self.currentStatus == "Paused."):       # Run while file is still being read or the reading is paused.
+                        bytesMessage = ser.readline().decode()[:-1]     # This should either be the start of a record, a new_rate command, or EOF
 
-                        if bytesMessage == 'transfer' and self.destination_folder != "":
-                            self.update_status("Data transfer in progress....")
-                            file = open(self.destination_folder + "/test_" + str(fileCounter) + ".fn", "w")
-                            sequenceNum = 0
-                            nextLine = ser.readline().decode()
-                            while nextLine != '\n':
-                                meta = nextLine
-                                sequence = ser.readline().decode()
-                                file.write(meta)
-                                file.write(sequence)
-                                sequenceNum += 1
-
-                                if sequenceNum == 4000:
-                                    file.close()
-                                    fileCounter += 1
-                                    sequenceNum = 0
-                                    file = open(self.destination_folder + "/test_" + str(fileCounter) + ".fn", "w")
-
-                                nextLine = ser.readline().decode()
-                            self.update_status("Data transfer complete! Awaiting new action...")
-                                
-
-                        if bytesMessage == 'new_rate':
+                        if bytesMessage == 'new_rate' and self.currentStatus == "Paused.":     # Only update rate when paused.
                             newBaud = ser.readline().decode()
                             self.update_status("Baud rate updated to " + newBaud)
                             ser.setBaudrate(newBaud)
+                        elif bytesMessage == 'pause':       # Pauses the transfer for a baud rate change
+                            self.update_status("Paused.")
+                        elif bytesMessage != "" and bytesMessage[0] == '>':     # Ensures that the first piece of data of a record is the metadata
+                            self.update_status("Data transfer in progress....")
+                            sequence = ser.readline().decode()[:-1]
+                            if sequence != "" and sequence[0] != '>':       # Ensures that next piece of data is DNA sequence
+                                file.write(bytesMessage + "\n")
+                                file.write(sequence + "\n")
+                                whiteline = ser.readline().decode()[:-1]
+                                if whiteline == "":     # Every third line should be a blankspace
+                                    file.write("\n")
+                                else:
+                                    self.update_status("Error, line should have been a whiteline...")
+                                    return
+                                
+                                sequenceNum += 1        # A record has successfully been written, increment SequenceNum
+                                if sequenceNum == backend.recordsPerFile:     # When backend.recordsPerFile records have been written, close file and start another one
+                                    file.close()
+                                    fileCounter += 1
+                                    file = open(self.destination_folder + "/" + curTime + "_file" + str(fileCounter) + ".fn", "w")
+                                    sequenceNum = 0
+                            else:
+                                self.update_status("Error, line should have been a dna sequence...")
+                                return
+                        elif bytesMessage != "":        # If data is not a command, new record then it must be bad.
+                            self.update_status("Error, Received bad line: " + bytesMessage)
+                            return
+                        elif bytesMessage == "" and self.currentStatus == "Data transfer in progress....":     # If no data is received when not paused must be EOF
+                            stillReading = False
+                            file.close()
+                            self.update_status("Data transfer complete! Awaiting new action...")
+
+                elif bytesMessage == 'connect' and self.destination_folder == "":
+                    self.update_status("Error, must set folder before sending data.")
+                                
+
+                        
+                time.sleep(.01)
 
 
 if __name__ == '__main__':
@@ -89,7 +113,11 @@ if __name__ == '__main__':
     # Get QML File context
     backend = Backend()
     engine.rootObjects()[0].setProperty('backend', backend)
-    backend.readSerial()
+
+    backend.update_status("Awaiting Connection")
+    thread = threading.Thread(target=backend.readSerial, args=())
+    thread.start()
+    #backend.readSerial(ser)
 
     sys.exit(app.exec_())
 
